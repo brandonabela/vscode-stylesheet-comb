98790858e09b0267edb01eb17e2eab8398a2b6b2
Brandon Abela <brandonabela9@gmail.com>
2023-08-06 10:41:40 +0200

    Update runTest.ts

--- src/test/runTest.ts
+++ src/test/runTest.ts
@@ -6,13 +6,14 @@ async function main() {
     // The folder containing the Extension Manifest package.json
     const extensionDevelopmentPath = path.resolve(__dirname, '../../');
 
-    // The path to the test runner
+    // The path to test runner
+    // Passed to --extensionTestsPath
     const extensionTestsPath = path.resolve(__dirname, './suite/index');
 
-    // Download VS Code, unzip it, and run the integration tests
+    // Download VS Code, unzip it and run the integration test
     await runTests({ extensionDevelopmentPath, extensionTestsPath });
   } catch (err) {
-    console.error('Failed to run tests:', err);
+    console.error('Failed to run tests');
     process.exit(1);
   }
 }
